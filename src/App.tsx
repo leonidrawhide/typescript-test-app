6b7652dc071b0f1b80ad7929f5bbc0b61e590401
Leonid (notebook) <leonidrawhide@gmail.com>
2022-02-22 18:10:11 +0300

    test task for learning typescript

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import './App.css';
+import { InputField } from './components/InputField';
+import { TodoList } from './components/TodoList';
+import { Todo } from './model';
+
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([...todos, {id: Date.now(), todo, isDone: false}])
+      setTodo("")
+    }
+    
+  };
+
+  console.log(todos)
+
+  return (
+    <div className="App">
+      <span className="heading">Сделай это!</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+      <TodoList todos={todos} setTodos={setTodos} />
+      {/* {todos.map((item) => (
+        <li>{item.todo}</li>
+      ))} */}
+    </div>
+  );
+}
+
+export default App;
